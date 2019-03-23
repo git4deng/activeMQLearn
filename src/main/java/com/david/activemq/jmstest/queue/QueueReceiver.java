e632d2f3f576dbafa2c918d861e31b7630bf27af
yuanwei <wei.yuan@gh-ca.com>
2019-03-23 22:43:15 +0800

    消息确认机制的演示

--- src/main/java/com/david/activemq/jmstest/queue/QueueReceiver.java
+++ src/main/java/com/david/activemq/jmstest/queue/QueueReceiver.java
@@ -13,6 +13,79 @@ import javax.jms.TextMessage;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 public class QueueReceiver {
+	/**
+	 * 事务性会话的消息接收确认，当在事务会话中，一个事务提交的时候，消息的确认自动发生。
+	 * @param args
+	 * @throws Exception
+	 */
+//	public static void main(String[] args) throws Exception {
+//		//创建连接工厂
+//		ConnectionFactory connFactory=new ActiveMQConnectionFactory("tcp://47.107.184.235:61616");
+//		Connection conn = connFactory.createConnection();
+//		//连接 activeMQ
+//		conn.start();
+//		//查看jms定义的属性
+//		Enumeration names = conn.getMetaData().getJMSXPropertyNames();
+//		while(names.hasMoreElements()){
+//			String name = (String) names.nextElement();
+//			System.out.println("jms name----->"+name);
+//		}
+//		Session session = conn.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
+//		Destination destination = session.createQueue("david-queue");
+//		
+//		MessageConsumer consumer = session.createConsumer(destination);
+//		int i=0;
+//		while(i<3){
+//			
+//			//TextMessage message=(TextMessage) consumer.receive();
+//			MapMessage message=(MapMessage) consumer.receive();
+//			//事务提交的时候，消息自动确认
+//			session.commit();
+//		//	System.out.println("接收到消息了："+message.getText());
+//			System.out.println("收到消息："+message.getString("message---"+i)+",property:"+message.getStringProperty("hello"+i));
+//			i++;
+//		}
+//		session.close();
+//		conn.close();
+//	}
+	/**
+	 * 当消息处于非事务会话中，消息何时被确认取决于会话创建时候选择的应答模式 ,Session.AUTO_ACKNOWLEDGE，当客户端成功的从receive（）方法返回的时侯或者
+	 * 从MessageListener.onMessage方法成功返回时，会话自动确认客户收到消息
+	 * @param args
+	 * @throws Exception
+	 */
+//	public static void main(String[] args) throws Exception {
+//		//创建连接工厂
+//		ConnectionFactory connFactory=new ActiveMQConnectionFactory("tcp://47.107.184.235:61616");
+//		Connection conn = connFactory.createConnection();
+//		//连接 activeMQ
+//		conn.start();
+//		//查看jms定义的属性
+//		Enumeration names = conn.getMetaData().getJMSXPropertyNames();
+//		while(names.hasMoreElements()){
+//			String name = (String) names.nextElement();
+//			System.out.println("jms name----->"+name);
+//		}
+//		Session session = conn.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+//		Destination destination = session.createQueue("david-queue_auto_acknowledge");
+//		
+//		MessageConsumer consumer = session.createConsumer(destination);
+//		int i=0;
+//		while(i<3){
+//			MapMessage message=(MapMessage) consumer.receive();
+//			System.out.println("收到消息："+message.getString("message---"+i)+",property:"+message.getStringProperty("hello"+i));
+//			i++;
+//		}
+//		session.close();
+//		conn.close();
+//	}
+	/**
+	 * 当消息处于非事务会话中，消息何时被确认取决于会话创建时候选择的应答模式,Session.CLIENT_ACKNOWLEDGE，
+	 * 客户端需要调用消息的aclnowlege方法确认消息，注意，这个种模式的消息确认是会话层面的，一但一个消息被确认
+	 * 那么session下面的所有消息都会被确认。
+	 * @param args
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
 		//创建连接工厂
 		ConnectionFactory connFactory=new ActiveMQConnectionFactory("tcp://47.107.184.235:61616");
@@ -25,19 +98,16 @@ public class QueueReceiver {
 			String name = (String) names.nextElement();
 			System.out.println("jms name----->"+name);
 		}
-		Session session = conn.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
-		Destination destination = session.createQueue("david-queue");
+		Session session = conn.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+		Destination destination = session.createQueue("david-queue_client_acknowledge");
 		
 		MessageConsumer consumer = session.createConsumer(destination);
 		int i=0;
 		while(i<3){
-			
-			//TextMessage message=(TextMessage) consumer.receive();
 			MapMessage message=(MapMessage) consumer.receive();
-			
-			session.commit();
-		//	System.out.println("接收到消息了："+message.getText());
 			System.out.println("收到消息："+message.getString("message---"+i)+",property:"+message.getStringProperty("hello"+i));
+			//通过消息本身来确认消息
+			if(i==2) message.acknowledge();
 			i++;
 		}
 		session.close();
--- src/main/java/com/david/activemq/jmstest/queue/QueueSender.java
+++ src/main/java/com/david/activemq/jmstest/queue/QueueSender.java
@@ -11,33 +11,96 @@ import javax.jms.TextMessage;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 public class QueueSender {
+	/**
+	 * 事务性会话的消息接收确认，当在事务会话中，一个事务提交的时候，消息的确认自动发生。
+	 * @param args
+	 * @throws Exception
+	 */
+//	public static void main(String[] args) throws Exception {
+//		//创建连接工厂
+//		ConnectionFactory connFactory=new ActiveMQConnectionFactory("tcp://47.107.184.235:61616");
+//		Connection conn = connFactory.createConnection();
+//		//连接 activeMQ
+//		conn.start();
+//		//创建会话 Boolean.TRUE 表示需要会话 Session.AUTO_ACKNOWLEDGE 会话自动确认
+//		Session session = conn.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
+//		//创建目的地
+//		Destination destination = session.createQueue("david-queue");
+//		//创建生产者
+//		MessageProducer producer = session.createProducer(destination);
+//		//发送消息
+//		for (int i = 0; i < 3; i++) {
+//			/*TextMessage message = session.createTextMessage("message------>"+i);
+//			Thread.sleep(1000);
+//			*/
+//			// 测试message相关结构属性
+//			MapMessage mapMessage = session.createMapMessage();
+//			//设置属性
+//			mapMessage.setStringProperty("hello"+i, "world");
+//			//设置值
+//			mapMessage.setString("message---"+i, "my map message++++++++"+i);
+//			producer.send(mapMessage);
+//		}
+//		session.commit();
+//		session.close();
+//		conn.close();
+//	}
+	/**
+	 * 当消息处于非事务会话中，消息何时被确认取决于会话创建时候选择的应答模式,Session.AUTO_ACKNOWLEDGE，当客户端成功的从receive（）方法返回的时侯或者
+	 * 从MessageListener.onMessage方法成功返回时，会话自动确认客户收到消息
+	 * @param args
+	 * @throws Exception
+	 */
+//	public static void main(String[] args) throws Exception {
+//		//创建连接工厂
+//		ConnectionFactory connFactory=new ActiveMQConnectionFactory("tcp://47.107.184.235:61616");
+//		Connection conn = connFactory.createConnection();
+//		//连接 activeMQ
+//		conn.start();
+//		Session session = conn.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+//		//创建目的地
+//		Destination destination = session.createQueue("david-queue_auto_acknowledge");
+//		//创建生产者
+//		MessageProducer producer = session.createProducer(destination);
+//		//发送消息
+//		for (int i = 0; i < 3; i++) {
+//			/*TextMessage message = session.createTextMessage("message------>"+i);
+//			Thread.sleep(1000);
+//			 */
+//			// 测试message相关结构属性
+//			MapMessage mapMessage = session.createMapMessage();
+//			//设置属性
+//			mapMessage.setStringProperty("hello"+i, "world");
+//			//设置值
+//			mapMessage.setString("message---"+i, "my map message++++++++"+i);
+//			producer.send(mapMessage);
+//		}
+//		session.close();
+//		conn.close();
+//	}
+	/**
+	 * 当消息处于非事务会话中，消息何时被确认取决于会话创建时候选择的应答模式,Session.CLIENT_ACKNOWLEDGE，
+	 * 客户端需要调用消息的aclnowlege方法确认消息，注意，这个种模式的消息确认是会话层面的，一但一个消息被确认
+	 * 那么session下面的所有消息都会被确认。
+	 * @param args
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
-		//创建连接工厂
+		
 		ConnectionFactory connFactory=new ActiveMQConnectionFactory("tcp://47.107.184.235:61616");
 		Connection conn = connFactory.createConnection();
-		//连接 activeMQ
 		conn.start();
-		//创建会话 Boolean.TRUE 表示需要会话 Session.AUTO_ACKNOWLEDGE 会话自动确认
-		Session session = conn.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
-		//创建目的地
-		Destination destination = session.createQueue("david-queue");
-		//创建生产者
+		Session session = conn.createSession(Boolean.FALSE, Session.CLIENT_ACKNOWLEDGE);
+		Destination destination = session.createQueue("david-queue_client_acknowledge");
 		MessageProducer producer = session.createProducer(destination);
-		//发送消息
 		for (int i = 0; i < 3; i++) {
-			/*TextMessage message = session.createTextMessage("message------>"+i);
-			Thread.sleep(1000);
-			*/
-			// 测试message相关结构属性
 			MapMessage mapMessage = session.createMapMessage();
-			//设置属性
 			mapMessage.setStringProperty("hello"+i, "world");
-			//设置值
 			mapMessage.setString("message---"+i, "my map message++++++++"+i);
 			producer.send(mapMessage);
 		}
-		session.commit();
 		session.close();
 		conn.close();
 	}
+	
 }
